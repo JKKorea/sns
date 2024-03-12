640cabc4cd868fa4f2ac4f04df04c05190b3e0ca
jongkun.lee <jongkun7987@gmail.com>
2024-03-12 23:19:31 +0900

    test : fix TC

--- src/test/java/com/jk/sns/controller/PostControllerTest.java
+++ src/test/java/com/jk/sns/controller/PostControllerTest.java
@@ -1,5 +1,8 @@
 package com.jk.sns.controller;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -10,12 +13,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jk.sns.controller.request.PostModifyRequest;
 import com.jk.sns.controller.request.PostWriteRequest;
 import com.jk.sns.exception.ErrorCode;
+import com.jk.sns.exception.SimpleSnsApplicationException;
 import com.jk.sns.service.PostService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -68,6 +73,8 @@ public class PostControllerTest {
     @Test
     @WithMockUser
     void 포스트수정시_본인이_작성한_글이_아니라면_에러발생() throws Exception {
+        doThrow(new SimpleSnsApplicationException(ErrorCode.INVALID_PERMISSION)).when(postService)
+            .modify(any(), eq(1), eq("title"), eq("body"));
         mockMvc.perform(put("/api/v1/posts/1")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsBytes(new PostModifyRequest("title", "body"))))
@@ -78,16 +85,20 @@ public class PostControllerTest {
     @Test
     @WithMockUser
     void 포스트수정시_수정하려는글이_없다면_에러발생() throws Exception {
+        doThrow(new SimpleSnsApplicationException(ErrorCode.POST_NOT_FOUND)).when(postService)
+            .modify(any(), eq(1), eq("title"), eq("body"));
         mockMvc.perform(put("/api/v1/posts/1")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsBytes(new PostModifyRequest("title", "body"))))
             .andDo(print())
-            .andExpect(status().is(ErrorCode.INVALID_PERMISSION.getStatus().value()));
+            .andExpect(status().is(ErrorCode.POST_NOT_FOUND.getStatus().value()));
     }
 
     @Test
     @WithMockUser
     void 포스트수정시_데이터베이스_에러_발생시_에러발생() throws Exception {
+        doThrow(new SimpleSnsApplicationException(ErrorCode.DATABASE_ERROR)).when(postService)
+            .modify(any(), eq(1), eq("title"), eq("body"));
         mockMvc.perform(put("/api/v1/posts/1")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsBytes(new PostModifyRequest("title", "body"))))
@@ -107,7 +118,10 @@ public class PostControllerTest {
     @Test
     @WithMockUser
     void 포스트삭제시_본인이_작성한_글이_아니라면_에러발생() throws Exception {
+        doThrow(new SimpleSnsApplicationException(ErrorCode.INVALID_PERMISSION)).when(postService)
+            .delete(any(), eq(1));
         mockMvc.perform(delete("/api/v1/posts/1")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer token")
                 .contentType(MediaType.APPLICATION_JSON))
             .andDo(print())
             .andExpect(status().is(ErrorCode.INVALID_PERMISSION.getStatus().value()));
@@ -116,15 +130,21 @@ public class PostControllerTest {
     @Test
     @WithMockUser
     void 포스트삭제시_수정하려는글이_없다면_에러발생() throws Exception {
+        doThrow(new SimpleSnsApplicationException(ErrorCode.POST_NOT_FOUND)).when(postService)
+            .delete(any(), eq(1));
+
         mockMvc.perform(delete("/api/v1/posts/1")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer token")
                 .contentType(MediaType.APPLICATION_JSON))
             .andDo(print())
-            .andExpect(status().is(ErrorCode.INVALID_PERMISSION.getStatus().value()));
+            .andExpect(status().is(ErrorCode.POST_NOT_FOUND.getStatus().value()));
     }
 
     @Test
     @WithMockUser
     void 포스트삭제시_데이터베이스_에러_발생시_에러발생() throws Exception {
+        doThrow(new SimpleSnsApplicationException(ErrorCode.DATABASE_ERROR)).when(postService)
+            .delete(any(), eq(1));
         mockMvc.perform(delete("/api/v1/posts/1")
                 .contentType(MediaType.APPLICATION_JSON))
             .andDo(print())
--- src/test/java/com/jk/sns/service/PostServiceTest.java
+++ src/test/java/com/jk/sns/service/PostServiceTest.java
@@ -8,6 +8,7 @@ import com.jk.sns.exception.ErrorCode;
 import com.jk.sns.exception.SimpleSnsApplicationException;
 import com.jk.sns.fixture.UserEntityFixture;
 import com.jk.sns.model.entity.PostEntity;
+import com.jk.sns.model.entity.UserEntity;
 import com.jk.sns.repository.PostEntityRepository;
 import com.jk.sns.repository.UserEntityRepository;
 import java.util.Optional;
@@ -16,6 +17,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Pageable;
 
 @SpringBootTest
 public class PostServiceTest {
@@ -46,7 +48,6 @@ public class PostServiceTest {
     @Test
     void 포스트생성시_유저가_존재하지_않으면_에러를_내뱉는다() {
         String userName = "name";
-        String password = "password";
         String title = "title";
         String body = "body";
 
@@ -56,6 +57,105 @@ public class PostServiceTest {
             SimpleSnsApplicationException.class, () -> postService.create(userName, title, body));
 
         Assertions.assertEquals(ErrorCode.USER_NOT_FOUND, exception.getErrorCode());
+    }
+
+    @Test
+    void 내_포스트리스트를_가져올_유저가_존재하지_않으면_에러를_내뱉는다() {
+        String userName = "name";
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.empty());
+        SimpleSnsApplicationException exception = Assertions.assertThrows(
+            SimpleSnsApplicationException.class,
+            () -> postService.my(userName, mock(Pageable.class)));
+
+        Assertions.assertEquals(ErrorCode.USER_NOT_FOUND, exception.getErrorCode());
+    }
+
+    @Test
+    void 포스트_수정시_포스트가_존재하지_않으면_에러를_내뱉는다() {
+        Integer postId = 1;
+        String userName = "name";
+        String title = "title";
+        String body = "body";
+        when(postEntityRepository.findById(postId)).thenReturn(Optional.empty());
+        SimpleSnsApplicationException exception = Assertions.assertThrows(
+            SimpleSnsApplicationException.class,
+            () -> postService.modify(userName, postId, title, body));
+        Assertions.assertEquals(ErrorCode.POST_NOT_FOUND, exception.getErrorCode());
+    }
+
+    @Test
+    void 포스트_수정시_유저가_존재하지_않으면_에러를_내뱉는다() {
+        Integer postId = 1;
+        String userName = "name";
+        String title = "title";
+        String body = "body";
+        when(postEntityRepository.findById(postId)).thenReturn(Optional.of(mock(PostEntity.class)));
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.empty());
+        SimpleSnsApplicationException exception = Assertions.assertThrows(
+            SimpleSnsApplicationException.class,
+            () -> postService.modify(userName, postId, title, body));
+        Assertions.assertEquals(ErrorCode.USER_NOT_FOUND, exception.getErrorCode());
+    }
+
+
+    @Test
+    void 포스트_수정시_포스트_작성자와_유저가_일치하지_않으면_에러를_내뱉는다() {
+        PostEntity mockPostEntity = mock(PostEntity.class);
+        UserEntity mockUserEntity = mock(UserEntity.class);
+
+        Integer postId = 1;
+        String userName = "name";
+        String title = "title";
+        String body = "body";
+        when(postEntityRepository.findById(postId)).thenReturn(Optional.of(mockPostEntity));
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(mockUserEntity));
+        when(mockPostEntity.getUser()).thenReturn(mock(UserEntity.class));
+
+        SimpleSnsApplicationException exception = Assertions.assertThrows(
+            SimpleSnsApplicationException.class,
+            () -> postService.modify(userName, postId, title, body));
+        Assertions.assertEquals(ErrorCode.INVALID_PERMISSION, exception.getErrorCode());
+    }
+
+    @Test
+    void 포스트_삭제시_포스트가_존재하지_않으면_에러를_내뱉는다() {
+        Integer postId = 1;
+        String userName = "name";
+        when(postEntityRepository.findById(postId)).thenReturn(Optional.empty());
+        SimpleSnsApplicationException exception = Assertions.assertThrows(
+            SimpleSnsApplicationException.class, () -> postService.delete(userName, postId));
+        Assertions.assertEquals(ErrorCode.POST_NOT_FOUND, exception.getErrorCode());
+    }
+
+    @Test
+    void 포스트_삭제시_유저가_존재하지_않으면_에러를_내뱉는다() {
+        Integer postId = 1;
+        String userName = "name";
+        when(postEntityRepository.findById(postId)).thenReturn(Optional.of(mock(PostEntity.class)));
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.empty());
+
+        SimpleSnsApplicationException exception = Assertions.assertThrows(
+            SimpleSnsApplicationException.class, () -> postService.delete(userName, postId));
+
+        Assertions.assertEquals(ErrorCode.USER_NOT_FOUND, exception.getErrorCode());
+    }
+
+
+    @Test
+    void 포스트_삭제시_포스트_작성자와_유저가_일치하지_않으면_에러를_내뱉는다() {
+        PostEntity mockPostEntity = mock(PostEntity.class);
+        UserEntity mockUserEntity = mock(UserEntity.class);
+
+        Integer postId = 1;
+        String userName = "name";
+        when(postEntityRepository.findById(postId)).thenReturn(Optional.of(mockPostEntity));
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(mockUserEntity));
+        when(mockPostEntity.getUser()).thenReturn(mock(UserEntity.class));
+        
+        SimpleSnsApplicationException exception = Assertions.assertThrows(
+            SimpleSnsApplicationException.class, () -> postService.delete(userName, postId));
+
+        Assertions.assertEquals(ErrorCode.INVALID_PERMISSION, exception.getErrorCode());
     }
 
 }
